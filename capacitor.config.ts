984927b3923565ba53e7ec90f5909ae3240ee01f
Ivan Ojeda Vargas <iojedav@uts.edu.co>
2024-05-02 20:50:16 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ivan-ojeda-grupoE191-examen2',
+  webDir: 'www'
+};
+
+export default config;
